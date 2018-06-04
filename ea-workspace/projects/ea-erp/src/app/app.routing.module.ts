ffc77ca4be5f80fd36fb3bf2032123ce7324f6be
Paolo Galfione <paolo.galfione@softwareuno.it>
2018-06-04 16:40:02 +0200

    aggiunto prodotto

--- ea-workspace/projects/ea-erp/src/app/app.routing.module.ts
+++ ea-workspace/projects/ea-erp/src/app/app.routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RouteNav } from 'eal-core';
+// work-around, la versione corrente di A6 da un problema con il lazy load di una lib
+// https://github.com/angular/angular/issues/23358
 import { EaClienteModule } from 'eal-erp';
 
 export const routes = [
