859098a57843b764f05ee986b10bcbec9dc6a8fa
Paolo Galfione <paolo.galfione@softwareuno.it>
2018-06-04 15:42:11 +0200

    fix lib

--- ea-workspace/projects/eal-erp/src/public_api.ts
+++ ea-workspace/projects/eal-erp/src/public_api.ts
@@ -6,3 +6,6 @@ export * from './lib/ea-cliente/ea-cliente-edit/ea-cliente-edit.component';
 export * from './lib/ea-cliente/ea-cliente-list/ea-cliente-list.component';
 export * from './lib/ea-cliente/ea-cliente.module';
 export * from './lib/ea-cliente/ea-cliente.routing.module';
+/* export * from './lib/cliente/cliente-edit/cliente-edit.component';
+export * from './lib/cliente/cliente-list/cliente-list.component';
+export * from './lib/cliente/cliente.module'; */
