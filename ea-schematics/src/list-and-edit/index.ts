765b2f957de034b91f1f8a75563bfb4327851360
Paolo Galfione <paolo.galfione@softwareuno.it>
2018-06-04 14:22:37 +0200

    read config.yaml

--- ea-schematics/src/list-and-edit/index.ts
+++ ea-schematics/src/list-and-edit/index.ts
@@ -1,36 +1,36 @@
 import {
   Rule,
-  SchematicContext,
-  Tree,
   apply,
-  chain,
   mergeWith,
   template,
   url,
   move,
+  chain,
+  Tree,
+  SchematicContext,
 } from '@angular-devkit/schematics';
 import { dasherize, classify, camelize } from '@angular-devkit/core/src/utils/strings';
-
+import * as fs from 'fs';
 
 const stringUtils = { dasherize, classify, camelize };
 
-// Instead of `any`, it would make sense here to get a schema-to-dts package and output the
-// interfaces so you get type-safe options.
 export default function (options: any): Rule {
-  // The chain rule allows us to chain multiple rules and apply them one after the other.
+
+  const buffer = fs.readFileSync(options.config);
+  const yaml = require('js-yaml');
+  const source = yaml.load(buffer);
+
   return chain([
     (_tree: Tree, context: SchematicContext) => {
       // Show the options for this Schematics.
-      context.logger.info('My Full Schematic: ' + JSON.stringify(options));
+      context.logger.info('options: ' + JSON.stringify(options));
+      context.logger.info('source: ' + JSON.stringify(source));
     },
-
     mergeWith(apply(url('./files'), [
       template({
         ...stringUtils,
-        name: options.name,
+        ...source
       }),
       move(`projects/${options.project}/src/lib`)
-    ])),
-    
-  ]);
+    ]))]);
 }
