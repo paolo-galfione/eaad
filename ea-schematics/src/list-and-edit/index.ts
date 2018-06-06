4185a44e33a8fe8aa370af91544c2dcdf5542cd3
Paolo Galfione <paolo.galfione@softwareuno.it>
2018-06-06 13:39:44 +0200

    tree read non funge

--- ea-schematics/src/list-and-edit/index.ts
+++ ea-schematics/src/list-and-edit/index.ts
@@ -10,21 +10,28 @@ import {
   SchematicContext,
 } from '@angular-devkit/schematics';
 import { dasherize, classify, camelize } from '@angular-devkit/core/src/utils/strings';
-import * as fs from 'fs';
 
 const stringUtils = { dasherize, classify, camelize };
 
 export default function (options: any): Rule {
 
-  const buffer = fs.readFileSync(options.config);
-  const yaml = require('js-yaml');
-  const source = yaml.load(buffer);
+  let source: any;
 
   return chain([
     (_tree: Tree, context: SchematicContext) => {
       // Show the options for this Schematics.
       context.logger.info('options: ' + JSON.stringify(options));
-      context.logger.info('source: ' + JSON.stringify(source, null, 2));
+      const yaml = require('js-yaml');
+      const buffer = _tree.read(options.config);
+      context.logger.info('buffer: ' + buffer);
+      source = yaml.load(buffer);
+      context.logger.info(JSON.stringify(source, null, 2));
+      context.logger.info('name: '+source.name);
+      return _tree;
+    },
+    (_tree: Tree, context: SchematicContext) => {
+      context.logger.info('name: '+source.name);
+      return _tree;
     },
     mergeWith(apply(url('./files'), [
       template({
